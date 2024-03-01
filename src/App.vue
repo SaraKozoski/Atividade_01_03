d2d27f92133d7ba1f510667d23e9133405465a2e
SaraKozoski <sarakozoski@gmail.com>
2024-03-01 14:54:15 -0300

    Atividade-1- completa

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,43 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+ import { ref } from 'vue';
+ const contador = ref (0)
+  function incrementar() {
+    contador.value++
+  }
+  function decrementar() {
+    if (contador.value > 0){
+      contador.value --
+    }
+  }
+  function reiniciar(){
+    contador.value = 0;
+  }
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+   <div>
+    <h1>Números</h1>
+    <p>{{ contador }}</p>
+    <button @click="incrementar">+</button>
+    <button @click="decrementar">-</button>
+    <button @click="reiniciar">0</button>
+   </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+<style>
+ button{
+  background-color: rgb(119, 109, 111);
+  color: black;
+  margin: 10px;
+  width: 100px;
+  height: 100px;
+  font-size: 20px;
+  text-align: center;
+ }
+ div{
+  text-align: center;
+ }
+ p{
+  font-size: 30px;
+ }
 </style>
